a46ab9c5bcfc68d027bda3c8cc21f3358800f2e7
alimarief <alimarief97@gmail.com>
2023-12-26 22:25:54 +0700

    Slicing signup & signin

---
+++ lib/prisma.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "./../node_modules/.prisma/client/index.d";
+
+let prisma: PrismaClient;
+
+declare const globalThis: {
+  prisma: PrismaClient;
+};
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!globalThis.prisma) {
+    globalThis.prisma = new PrismaClient();
+  }
+
+  prisma = globalThis.prisma;
+}
+
+export default prisma;
